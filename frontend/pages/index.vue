6755b927c8729e60955d9039489ea75c899e8a6a
Karssou <alexandrelarue44@gmail.com>
2025-03-02 18:53:30 +0100

    Final update avant premier pull request

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -4,7 +4,7 @@ import { useAuthStore } from "~/stores/auth";
 const auth = useAuthStore();
 
 async function logout() {
-  await auth.logout();
+  auth.logout();
 }
 </script>
 
@@ -12,16 +12,10 @@ async function logout() {
   <NuxtLink class="navigation btn" to="/authentification"
     >Page Authentification</NuxtLink
   >
+  
 
   <button @click.prevent="logout">LOG-OUT</button>
 
-  <div v-if="auth.isAuthenticated">
-    <h1>Salut {{ auth.user?.username }}</h1>
-  </div>
-
-  <div v-else>
-    <h1>Aucun utilisateur</h1>
-  </div>
 </template>
 
 <style lang="scss" scoped>
