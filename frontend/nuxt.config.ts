b3f750d9fbaa6b026aeda27d41e827ee43e72ee0
Karssou <alexandrelarue44@gmail.com>
2025-03-16 12:35:18 +0100

    NOUVEAUTE :
    -Création complète du système de profil
    - Commencement de la navbar
    - Optimisation des dépendances comme Google Fonts qu'il va falloir remettre a jour
    - Côté Backend : Création des plusieurs validators pour la modification de profil, amélioration du model user
    -Nouveau système de gestion des îcones en local
    
    PROCHAINEMENT :
    
    -NAVBAR complète
    
    ERREURS A FIXE :
    
    - GESTION DE LA BIRTHDATE
    - SELECT BOX A AMELIORER

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -5,44 +5,9 @@ export default defineNuxtConfig({
       apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://localhost:3333",
     },
   },
-  modules: [
-    "@pinia/nuxt",
-    [
-      "@nuxtjs/google-fonts",
-      {
-        families: {
-          Lato: {
-            wght: [400, 700],
-            ital: [400, 700],
-            regular: [400, 700],
-          },
-          Opensans: { wght: [400, 600], ital: [400, 600] },
-          Ubuntu: { wght: [400, 500] },
-          Montserrat: {
-            wght: [400, 900],
-          },
-        },
-      },
-    ],
-    "@nuxt/icon",
-  ],
+  modules: ["@pinia/nuxt", "@nuxtjs/google-fonts", "@nuxt/icon"],
 
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: [
-    "@/assets/styles/main.scss",
-    "@/assets/styles/variables.scss",
-    "@/assets/styles/reset.scss",
-    "@/assets/fonts/fonts.css",
-  ],
-  googleFonts: {
-    useStylesheet: true,
-    preload: true,
-    preconnect: true,
-    prefetch: true,
-    overwriting: true,
-    outputDir: "assets/fonts",
-    fontsDir: "assets/fonts",
-    stylePath: "fonts.css",
-  },
+  css: ["@/assets/styles/main.scss", "@/assets/styles/reset.scss"],
 });
